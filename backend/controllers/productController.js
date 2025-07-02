a636684fac4eb75c2de5a0f6928dff4dacf6039a
keerthi723 <keerthiga342@gmail.com>
2025-07-02 13:01:43 +0530

    fixing bug
    fixing error

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -125,7 +125,7 @@ exports.deleteProduct = catchAsyncError(async (req, res, next) => {
     });
   }
 
-  await product.remove();
+  await Product.findByIdAndDelete(req.params.id);
 
   res.status(200).json({
     success: true,
