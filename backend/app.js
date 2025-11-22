8998d22d097aaf62f350486b98c3aae257a389e7
keerthi723 <keerthiga342@gmail.com>
2025-11-22 13:53:24 +0530

    Fix middleware order

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,6 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
-
-app.use(express.json());
-app.use(cookieParser());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(
   cors({
     origin: [
@@ -22,6 +18,9 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const products = require("./routes/product");
 const auth = require("./routes/auth");
